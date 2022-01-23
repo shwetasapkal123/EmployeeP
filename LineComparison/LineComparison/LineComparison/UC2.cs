19898567afb901947ee537f3f2d0e44c40789dad
shwetasapkal123 <96557730+shwetasapkal123@users.noreply.github.com>
2022-01-23 14:22:32 +0530

    Add comments

--- LineComparison/LineComparison/LineComparison/UC2.cs
+++ LineComparison/LineComparison/LineComparison/UC2.cs
@@ -29,13 +29,13 @@ namespace LineComparison
             Console.WriteLine("Enter Y4 point");            //take input from user
             y4= Convert.ToInt32(Console.ReadLine());        //read user input and convert it to integer
 
-            firstLength=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));
-            Console.WriteLine("first Length is:" + Math.Round(firstLength, 2));
-            secLength=Math.Sqrt(Math.Pow(x4-x3,2)+Math.Pow(y4-y3,2));
-            Console.WriteLine("Second Length is :"+Math.Round(secLength, 2));
+            firstLength=Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));     //Calculating length of first two lines
+            Console.WriteLine("first Length is:" + Math.Round(firstLength, 2)); //Result of line round to two digit after decimal and display 
+            secLength=Math.Sqrt(Math.Pow(x4-x3,2)+Math.Pow(y4-y3,2));           //Calculating length
+            Console.WriteLine("Second Length is :"+Math.Round(secLength, 2));   //Result round two two decimal points and display 
 
             Console.WriteLine("\nUsing Equal Method\n");
-            if (firstLength.Equals(secLength))
+            if (firstLength.Equals(secLength))                              //Checking first length and second length equal or not and display
                 Console.WriteLine("Length of the line is equal");
             else
                 Console.WriteLine("length of the line is not equal");
