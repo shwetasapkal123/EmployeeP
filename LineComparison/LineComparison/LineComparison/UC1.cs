2c0a89464b492d8428f87d8a75adae3ec89c8476
shwetasapkal123 <96557730+shwetasapkal123@users.noreply.github.com>
2022-01-23 14:42:58 +0530

    Add comments

--- LineComparison/LineComparison/LineComparison/UC1.cs
+++ LineComparison/LineComparison/LineComparison/UC1.cs
@@ -8,12 +8,12 @@ namespace LineComparison
 {
      class UC1
     {
-        public void CalLength(double x1, double y1, double x2, double y2)
+        public void CalLength(double x1, double y1, double x2, double y2)       //method create
         {
-            double a = Math.Sqrt(Math.Pow(x2 - x1,2));
-            double b = Math.Sqrt(Math.Pow(y2 - y1,2));
-            double c = a + b;
-            Console.WriteLine("Length of a line is :" + c);
+            double a = Math.Sqrt(Math.Pow(x2 - x1,2));           //calculating length (x2-x1)^2
+            double b = Math.Sqrt(Math.Pow(y2 - y1,2));           //Calculating (y2-y1)^2
+            double c = a + b;                                    //Calculating length
+            Console.WriteLine("Length of a line is :" + c);      //display the output
         }
     }
 }
