8689c7c186eeb6fe69b0d0be99973a6490293a71
shwetasapkal123 <sapkalshweta30797@gmail.com>
2022-01-28 20:38:40 +0530

    Assignment

---
+++ HeadTail/HeadTail/Headtail.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadTail
+{
+     class Headtail
+    {
+        static void Main(string[] args)
+        {
+            int heads = 0, tails = 0;
+            Console.WriteLine("Enter how many times want to flip coin");
+            int n=Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+
+            for (int i = 0; i <= n; i++)
+            {
+                double randomInput = random.NextDouble();
+                Console.WriteLine("random input" + randomInput);
+                if(randomInput<=0.5)
+                {
+                    if(randomInput==0)
+                    {
+                        Console.WriteLine("Heads");
+                        heads+=1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Tails");
+                        tails += 1;
+                    }
+                }
+            }
+            Console.WriteLine("Total number of Heads:{0} and Tails{1}", heads, tails);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HeadTail/Powertwo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Powertwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number upto which we want power of two:");
+            int n = Convert.ToInt32(Console.ReadLine());
+                       
+                if (n>=0 && n<31)
+                {
+                    double pow = Math.Pow(2, n);
+                    Console.WriteLine("2 power {0} is {1}", n, pow);
+                }
+                else
+                {
+                    Console.WriteLine("Entered number not valid");
+                }
+            
+            Console.ReadLine();
+        }
+    }
+}
